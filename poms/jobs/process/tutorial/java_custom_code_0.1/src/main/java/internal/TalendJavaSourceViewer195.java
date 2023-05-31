703bcf87f9ce19f5dfe2225ef42e901539e24028
youssefjn <youssefjneyah@gmail.com>
2023-05-31 16:01:45 +0100

    Added 2 jobs and some updates

---
+++ poms/jobs/process/tutorial/java_custom_code_0.1/src/main/java/internal/TalendJavaSourceViewer195.java
@@ -0,0 +1,32 @@
+package internal;
+
+import routines.DataOperation;
+import routines.Mathematical;
+import routines.Numeric;
+import routines.Relational;
+import routines.StringHandling;
+import routines.TalendDataGenerator;
+import routines.TalendDate;
+import routines.TalendStringUtil;
+import routines.TalendString;
+import routines.system.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.math.BigDecimal;
+
+@SuppressWarnings("unused")
+
+public class TalendJavaSourceViewer195 {
+	private static java.util.Properties context = new java.util.Properties();
+	private static final java.util.Map<String, Object> globalMap = new java.util.HashMap<String, Object>();
+	public void myFunction(){
+	  if( 
+
+
+){
+	}
+	
+}
+}
